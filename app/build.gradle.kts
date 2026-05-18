a0196e178d6615710bdbabc20e09bedbf982466d
ilia <ilia.iliev@h-tech.tv>
2026-05-18 13:58:46 +0300

    Bump versionCode to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.nodepiazza"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
+        versionCode = 8
         versionName = "0.1.5"
 
         ndk {
