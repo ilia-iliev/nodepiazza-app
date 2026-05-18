78e79aa58fe980d48095a21daa24b3543640b0b6
ilia <ilia.iliev@h-tech.tv>
2026-05-18 11:52:36 +0300

    Bump to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.nodepiazza"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
 
         ndk {
             debugSymbolLevel = "FULL"
